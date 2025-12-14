e51f48ab5dbdaf1862cec41bbb91b08125804a05
傅朝俊 <fuchaojun@szkingdom.com>
2025-12-14 21:37:56 +0800

    fix：2.2 demo图片定时切换（complete on mouse leave and enter）

--- 2. React核心概念/2.2 demo图片定时切换/react-learn/src/index.js
+++ 2. React核心概念/2.2 demo图片定时切换/react-learn/src/index.js
@@ -1,44 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import img1 from "./assets/1.jpg";
-import img2 from "./assets/2.jpg";
-import img3 from "./assets/3.jpg";
+import src1 from "./assets/1.jpg";
+import src2 from "./assets/2.jpg";
+import src3 from "./assets/3.jpg";
+// vue 可以直接在 img元素写 "./assets/1.jpg"，但 react 不行，因为打包后路径会变，所有需要用 import
+
+const container = document.getElementById("root");
 
 let index = 0;
-const imgArr = [img1, img2, img3];
-let div;
-let timer = null;
+const imgArr = [src1, src2, src3];
+let timer;
 init();
 
 
 function init() {
-  setInterval(() => {
+  timer = setInterval(() => {
     render();
-    if (divDom) {
-      console.log('reg');
-      registerEvent();
-    }
   }, 1000);
 }
 
 function render() {
   index = (index + 1) % imgArr.length;
-  div = (
-    <div className={"container"}>
-      <img className={"img"} src={imgArr[index]} alt=""/>
-    </div>
-  );
-  divDom = ReactDOM.render(div, document.getElementById("root"));
+  ReactDOM.render(<img className={"img"} src={imgArr[index]} alt=""/>, container);
 }
 
-let divDom = null;
-function registerEvent() {
-  divDom.onMouseEnter = function () {
-    clearInterval(timer);
-    timer = null;
-  };
-  divDom.onMouseLeave = function () {
-    init();
-  };
-}
+container.onmouseenter = function () {
+  console.log('enter');
+  clearInterval(timer);
+  timer = null;
+};
+
+container.onmouseleave = function () {
+  init();
+};
