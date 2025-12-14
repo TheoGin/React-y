9d76b809502050d481d9b4e528ee3e8418e347c0
傅朝俊 <fuchaojun@szkingdom.com>
2025-12-14 21:26:17 +0800

    feat：2.2 demo图片定时切换（incomplete on mouse leave and enter）

--- 2. React核心概念/2.2 demo图片定时切换/react-learn/src/index.js
+++ 2. React核心概念/2.2 demo图片定时切换/react-learn/src/index.js
@@ -1,51 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import src1 from "./assets/1.jpg";
-import src2 from "./assets/2.jpg";
-import src3 from "./assets/3.jpg";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
+import img1 from "./assets/1.jpg";
+import img2 from "./assets/2.jpg";
+import img3 from "./assets/3.jpg";
 
-const srcs = [src1, src2, src3]; //保存图片路径的数组
+let index = 0;
+const imgArr = [img1, img2, img3];
+let div;
+let timer = null;
+init();
 
-let index = 0; //显示的图片索引
 
-const container = document.getElementById('root');
+function init() {
+  setInterval(() => {
+    render();
+    if (divDom) {
+      console.log('reg');
+      registerEvent();
+    }
+  }, 1000);
+}
 
-let timer; //计时器
-
-/**
- * 根据index的值，显示某张图片
- */
 function render() {
-    ReactDOM.render(<img src={srcs[index]} alt="" />, container);
+  index = (index + 1) % imgArr.length;
+  div = (
+    <div className={"container"}>
+      <img className={"img"} src={imgArr[index]} alt=""/>
+    </div>
+  );
+  divDom = ReactDOM.render(div, document.getElementById("root"));
 }
 
-/**
- * 启动计时器，每隔一段时间，切换图片
- */
-function start() {
-    stop();
-    timer = setInterval(() => {
-        index = (index + 1) % 3; //改变index
-        render();
-    }, 2000);
-}
-
-/**
- * 停止计时器
- */
-function stop(){
+let divDom = null;
+function registerEvent() {
+  divDom.onMouseEnter = function () {
     clearInterval(timer);
-}
-
-render();
-
-start();
-
-container.onmouseenter = function(){
-    stop();
-}
-
-container.onmouseleave = function(){
-    start();
+    timer = null;
+  };
+  divDom.onMouseLeave = function () {
+    init();
+  };
 }
