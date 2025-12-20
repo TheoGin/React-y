d3f01d043d62ddcf212407c93212e254e5228d1d
傅朝俊 <fuchaojun@szkingdom.com>
2025-12-20 17:39:39 +0800

    init: 2.5 组件状态

---
+++ 2. React核心概念/2.5 组件状态/react-learn/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Tick from "./components/Tick"
+import A from "./components/Test"
+
+// ReactDOM.render(<Tick number={10} />, document.getElementById('root'));
+
+ReactDOM.render(<A/>, document.getElementById('root'))
+
