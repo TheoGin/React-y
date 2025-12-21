f31a5d409082cf35578497d4674227da4643dce2
傅朝俊 <fuchaojun@szkingdom.com>
2025-12-21 21:45:15 +0800

    fix: 2.6 demo自动移动的小球(after complete free move)

--- 2. React核心概念/2.6 demo自动移动的小球/react-learn/src/components/Ball.js
+++ 2. React核心概念/2.6 demo自动移动的小球/react-learn/src/components/Ball.js
@@ -1,33 +1,40 @@
 import React from "react";
 import "./ball.css";
 
+/**
+ * 一个能够自动移动的小球
+ */
 class Ball extends React.Component {
 
   constructor(props) {
-    console.log(props);
+    // console.log(props);
     super(props);
+
+    // 属性中需要分别传递横纵坐标上的速度，每秒移动的像素值
+    // props.xSpeed,  props.ySpeed
+    // 需要传递背景颜色，如果没有传递，则使用蓝色
     this.state = {
-      left: props.left || 0,
-      top: props.top || 0,
+      left: props.left || 0, // 横坐标
+      top: props.top || 0, // 纵坐标
       xSpeed: props.speed || 300,
       ySpeed: props.speed || 300,
-      bg: props.bg || "blue",
       size: props.size || 100,
     };
 
-    const duration = 16;
+    const duration = 16; // 间隔的毫秒数
     setInterval(() => {
       const html = document.documentElement;
       // 位移 = 速度 * 时间 （秒）
       const xDis = this.state.xSpeed * duration / 1000; // 除以 1000 是将毫秒转化为秒
       const yDis = this.state.ySpeed * duration / 1000;
+
+      // 根据速度，改变left和top值
       let newLeft = this.state.left + xDis;
       let newTop = this.state.top + yDis;
       if (newLeft <= 0) {
         newLeft = 0;
-        // 改变速度的方向
         this.setState({
-          xSpeed: -this.state.xSpeed,
+          xSpeed: -this.state.xSpeed, // 横坐标反向
         });
 
         // Do not mutate state directly. Use setState()
@@ -36,19 +43,19 @@ class Ball extends React.Component {
         newLeft = html.clientWidth - this.state.size;
         // 改变速度的方向
         this.setState({
-          xSpeed: -this.state.xSpeed,
+          xSpeed: -this.state.xSpeed, // 横坐标反向
         });
       }
 
       if (newTop <= 0) {
         newTop = 0;
         this.setState({
-          ySpeed: -this.state.ySpeed,
+          ySpeed: -this.state.ySpeed, // 纵坐标反向
         });
       } else if (newTop >= html.clientHeight - this.state.size) {
         newTop = html.clientHeight - this.state.size;
         this.setState({
-          ySpeed: -this.state.ySpeed,
+          ySpeed: -this.state.ySpeed, // 纵坐标反向
         });
       }
       // console.log(newLeft, newTop);
@@ -68,7 +75,7 @@ class Ball extends React.Component {
           // left: this.state.left,
           // top: this.state.top,
           transform: `translate(${this.state.left}px, ${this.state.top}px)`,
-          backgroundColor: this.state.bg,
+          backgroundColor: this.props.bg || 'blue',
           // backgroundColor: `rgb(192, 168, ${this.props.rgb})`,
           width: this.props.size,
           height: this.props.size,
--- 2. React核心概念/2.6 demo自动移动的小球/react-learn/src/index.js
+++ 2. React核心概念/2.6 demo自动移动的小球/react-learn/src/index.js
@@ -1,8 +1,5 @@
 import ReactDOM from "react-dom";
 import React from 'react'
 import BallList from "./components/BallList";
-function getRandom(min, max) {
-  return min + Math.floor(Math.random() * (max - min))
-}
-window.getRandom = getRandom
+
 ReactDOM.render(<BallList />, document.getElementById('root'))
