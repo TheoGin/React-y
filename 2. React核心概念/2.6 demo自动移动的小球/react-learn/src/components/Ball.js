0975e96c5ad8665f5cb5b5c1b04f9ee92849688d
傅朝俊 <fuchaojun@szkingdom.com>
2025-12-21 20:29:13 +0800

    feat: 2.6 demo自动移动的小球(after complete free move)

--- 2. React核心概念/2.6 demo自动移动的小球/react-learn/src/components/Ball.js
+++ 2. React核心概念/2.6 demo自动移动的小球/react-learn/src/components/Ball.js
@@ -1,39 +1,49 @@
 import React from "react";
-import './ball.css'
-import {getRandom} from "./util";
+import "./ball.css";
+
 class Ball extends React.Component {
-  state ={
+  state = {
     left: this.props.left,
     top: this.props.top,
-  }
+    leftSpeed: this.props.speed || 30,
+    topSpeed: this.props.speed || 30,
+    size: this.props.size || 50,
+  };
+
   constructor(props) {
     super(props);
     const html = document.documentElement;
-    // const left = getRandom(0, html.clientWidth - this.props.size);
-    // const top = getRandom(0, html.clientHeight - this.props.size);
-    setInterval(()=> {
-      const left = this.state.left + 5;
-      if (left >= html.clientWidth) {
-        this.state.left = getRandom(0, html.clientWidth - this.props.size)
+    setInterval(() => {
+      const left = this.state.left + this.state.leftSpeed;
+      if (left >= html.clientWidth - this.state.size) {
+        this.state.leftSpeed = -this.state.leftSpeed;
       }
-      const top = this.state.top + 5;
-      console.log(left, top);
-      if (top >= html.clientHeight) {
-        this.state.top = getRandom(0, html.clientHeight - this.props.size)
+      if (left <= 0) {
+        this.state.leftSpeed = -this.state.leftSpeed;
+      }
+      const top = this.state.top + this.state.topSpeed;
+      // console.log(left, top);
+      if (top >= html.clientHeight - this.state.size) {
+        this.state.topSpeed = -this.state.topSpeed;
+      }
+      if (top <= 0) {
+        this.state.topSpeed = -this.state.topSpeed;
       }
       this.setState({
         left,
         top,
-      })
-    }, 100)
+      });
+    }, 100);
   }
+
   render() {
     return (
       <div className={"ball"} style={
         {
-          left: this.state.left + 'px',
-          top: this.state.top + 'px',
-          backgroundColor: `rgb(192, 168, ${this.props.rgb})`
+          /*left: this.state.left + "px",
+          top: this.state.top + "px",*/
+          transform: `translate(${this.state.left}px, ${this.state.top}px)`,
+          backgroundColor: `rgb(192, 168, ${this.props.rgb})`,
         }
       }></div>
     );
