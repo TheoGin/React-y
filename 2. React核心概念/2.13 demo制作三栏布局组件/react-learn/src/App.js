1a786532416904d9eaf51f950029b1f5c58ba828
傅朝俊 <fuchaojun@szkingdom.com>
2026-01-10 22:05:09 +0800

    fix: 2.13 demo制作三栏布局组件(after listen)

--- 2. React核心概念/2.13 demo制作三栏布局组件/react-learn/src/App.js
+++ 2. React核心概念/2.13 demo制作三栏布局组件/react-learn/src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import ThreeLayout from "./components/common/ThreeLayout";
-import './App.css'
 
-function App(props) {
+function App() {
   return (
-    <div className='app-container'>
-      <ThreeLayout left={<div className='app-container-left'>左边栏</div>} right={<div className='app-container-right'>右边栏</div>}>
-        <div className='app-container-main'>
+    <div className="app-container">
+      <ThreeLayout
+        gap={50}
+        left={<div style={{
+          border: "1px solid #008c8c",
+          // ThreeLayout 组件内部的 aside-left 是弹性项，默认纵轴拉伸，才是等高。而传递元素不是弹性项，需要等高可以设置 height: '100%'
+          // height: "100%",
+        }}>左边栏</div>}
+        right={<div style={{
+          border: "1px solid #008c8c",
+        }}>右边栏</div>}
+      >
+        <div style={{
+          border: "1px solid #f40",
+        }}>
           <h1>主区域</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto consequatur consequuntur culpa cupiditate ducimus earum eius eligendi, eos facere id iure maiores obcaecati omnis possimus quas reprehenderit tenetur voluptas.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto consequatur consequuntur culpa
+            cupiditate ducimus earum eius eligendi, eos facere id iure maiores obcaecati omnis possimus quas
+            reprehenderit tenetur voluptas.
           </p>
         </div>
       </ThreeLayout>
--- 2. React核心概念/2.13 demo制作三栏布局组件/react-learn/src/components/common/ThreeLayout/index.js
+++ 2. React核心概念/2.13 demo制作三栏布局组件/react-learn/src/components/common/ThreeLayout/index.js
@@ -1,17 +1,30 @@
 import React from "react";
-import './index.css'
+import "./index.css";
 
 function ThreeLayout(props) {
+  const defaultProps = {
+    leftWidth: 200,
+    rightWidth: 200,
+    minWidth: 800,
+    gap: 0, // 间隙
+  };
+  const datas = Object.assign({}, defaultProps, props);
   return (
-    <div className='three-layout-container'>
-      <div className="left">
-        {props.left}
-      </div>
+    <div className="three-layout-container">
       <div className="main">
-        {props.children}
+        {datas.children}
       </div>
-      <div className="right">
-        {props.right}
+      <div className="aside-left" style={{
+        width: datas.leftWidth,
+        marginRight: datas.gap,
+      }}>
+        {datas.left}
+      </div>
+      <div className="aside-right" style={{
+        width: datas.rightWidth,
+        marginLeft: datas.gap,
+      }}>
+        {datas.right}
       </div>
     </div>
   );
