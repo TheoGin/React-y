f038806c17cc95c61b08cba81033c0af54d47fcb
傅朝俊 <fuchaojun@szkingdom.com>
2026-01-20 19:09:44 +0800

    init: 3.8 Context

---
+++ 3. React进阶/3.8 Context/react-learn/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Test from "./components/common/Select/Test";
+// import Test from "./components/common/CheckboxGroup/Test";
+import Test from "./components/common/RadioBoxGroup/Test";
+
+function App(props) {
+  return (
+    <div>
+     <Test/>
+    </div>
+  );
+}
+
+export default App;
