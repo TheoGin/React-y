d9bc024d6480db347a810ce1081df0ef8ee75f6d
傅朝俊 <fuchaojun@szkingdom.com>
2026-02-08 20:53:39 +0800

    feat: 4.7 Context Hook

--- 4. HOOK/4.7 Context Hook/react-learn/src/App.js
+++ 4. HOOK/4.7 Context Hook/react-learn/src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+
+const context = React.createContext();
+
+/* function Test() {
+  // 会导致组件层级变深
+  return <context.Consumer>
+    { value => {
+      return <h1>Text, 上下文的值：{ value }</h1>;
+    } }
+  </context.Consumer>;
+} */
+function Test() {
+  // function useContext<T>(context: Context<T>): T; 用于获取上下文数据
+  const value = useContext(context);
+  return <h1>Text, 上下文的值：{ value }</h1>;
+}
 
 function App() {
 
   return (
-    <div>
-     1
-    </div>
+    <context.Provider value="abc">
+      <Test />
+    </context.Provider>
   );
 }
 
