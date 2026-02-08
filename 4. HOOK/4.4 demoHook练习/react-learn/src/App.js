5c0bfc2275cd168e2eaef8fd6b4b9cbf4cd82302
傅朝俊 <fuchaojun@szkingdom.com>
2026-02-08 10:49:03 +0800

    init: 4.4 demoHook练习

---
+++ 4. HOOK/4.4 demoHook练习/react-learn/src/App.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+import { fetchStudent } from "./services/student";
+
+class App extends PureComponent {
+
+
+  render() {
+    return (1
+    );
+  }
+}
+
+export default App;
