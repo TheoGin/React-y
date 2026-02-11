e89555d1f47d2e6e409d5f0932029d4691837fbf
傅朝俊 <fuchaojun@szkingdom.com>
2026-02-11 22:35:11 +0800

    feat: 4.12 layoutEffectHook

--- 4. HOOK/4.12 layoutEffectHook/react-learn/src/App.js
+++ 4. HOOK/4.12 layoutEffectHook/react-learn/src/App.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
+
+/* var div = document.querySelector("#root");
+ div.innerHTML = "abc";
+ div.innerHTML = "123";
+ div.style.color = "red";
+ // 上面三句会一起运行，而不是分三次 */
+
 
 function App() {
+  const [num, setNum] = useState(1);
+  const h1Ref = useRef();
+
+  /* // useEffect 用户看到新的效果，会导致闪动
+  useEffect(() => {
+    // setNum(Math.random()); // 会导致栈溢出
+
+    h1Ref.current.innerText = Math.random().toFixed(2);
+  }); */
+
+  // useEffect 用户看到新的效果之前，不会导致闪动
+  useLayoutEffect(() => {
+    // setNum(Math.random()); // 会导致栈溢出
+
+    h1Ref.current.innerText = Math.random().toFixed(2);
+  })
 
   return (
-    <div>123</div>
+    <div>
+      <h1 ref={ h1Ref }>{ num }</h1>
+      <button onClick={ () => {
+        setNum(num + 1);
+      } }>+
+      </button>
+    </div>
   );
 }
 
