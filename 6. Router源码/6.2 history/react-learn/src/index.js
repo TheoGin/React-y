2c20b6094663b121f23ade93f010d55a89b5b3dd
傅朝俊 <fuchaojun@szkingdom.com>
2026-02-23 23:07:02 +0800

    feat: 6.2 history

--- 6. Router源码/6.2 history/react-learn/src/index.js
+++ 6. Router源码/6.2 history/react-learn/src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
 import "./mock";
+import './react-router/memoryHistory'
 
 ReactDOM.render(<App/>, document.getElementById("root"));
---
+++ 6. Router源码/6.2 history/react-learn/src/react-router/memoryHistory.js
@@ -0,0 +1,17 @@
+import { createMemoryHistory } from "history";
+
+// 6.2.4 createMemoryHistory 创建一个使用内存中的地址栈的history对象，一般用于没有地址栏的环境。配置对象：
+const history = createMemoryHistory({
+  initialEntries: ["/", "/abc"], // 表示初始数组内容
+  initialIndex: 0, // 默认指针指向的数组下标
+});
+
+
+window.h = history;
+// window.createMemoryHistory = createMemoryHistory;
+// h // {length: 2, action: 'POP', location: {…}, index: 0, entries: Array(2),…}
+// h.goForward(); // undefined
+// h.location // {pathname: '/abc', search: '', hash: '', state: undefined, key: '27i4bz'}
+// h.goBack() // undefined
+// h.location // {pathname: '/', search: '', hash: '', state: undefined, key: 'xcee31'}
+
