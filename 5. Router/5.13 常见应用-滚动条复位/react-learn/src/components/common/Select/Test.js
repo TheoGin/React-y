e7dfe3aa28bbafb5a289c0cd7e9bc4d53584da6d
傅朝俊 <fuchaojun@szkingdom.com>
2026-02-23 15:03:59 +0800

    init: 5.13 常见应用-滚动条复位

---
+++ 5. Router/5.13 常见应用-滚动条复位/react-learn/src/components/common/Select/Test.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import Select from "./index";
+import {getAllStudents} from "../../../services/student";
+
+class Test extends Component {
+  state = {
+    datas: [],
+    value: '',
+  };
+
+  async componentDidMount() {
+    const datas = await getAllStudents();
+    this.setState({
+      datas: datas.map(item => ({
+        value: item.id.toString(), // 传递的是数字，组件内部勾选，拿到 value={item.value} 是字符串，所以需要转为字符串
+        text: item.name,
+      }))
+    });
+  }
+
+  render() {
+    return (<div>
+      <Select
+        name="loves"
+        datas={this.state.datas}
+        value={this.state.value}
+        onChange={(value) => {
+          this.setState({
+            value,
+          });
+        }}
+      />
+    </div>);
+  }
+}
+
+export default Test;
