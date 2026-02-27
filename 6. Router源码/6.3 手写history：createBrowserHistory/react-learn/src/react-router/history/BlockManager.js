7d619640e06600e916191b42d9acfebfc56334a4
傅朝俊 <fuchaojun@szkingdom.com>
2026-02-27 14:07:47 +0800

    fix: 6.3 手写history：createBrowserHistory 2

--- 6. Router源码/6.3 手写history：createBrowserHistory/react-learn/src/react-router/history/BlockManager.js
+++ 6. Router源码/6.3 手写history：createBrowserHistory/react-learn/src/react-router/history/BlockManager.js
@@ -1,19 +1,16 @@
 export class BlockManager {
-  prompt = null;
+
+  prompt = null; // 该属性是否有值，决定了是否有阻塞
 
   constructor(getUserConfirmation) {
     this.getUserConfirmation = getUserConfirmation;
   }
 
+  /**
+   * 设置一个阻塞，传递一个提示消息
+   * @param {*} prompt 可以是字符串，也可以一个函数，函数返回一个消息字符串
+   */
   block(prompt) {
-    /* if (typeof prompt === "string") {
-      this.prompt = prompt;
-    } else if (typeof prompt === "function") {
-      this.prompt = prompt(location, action); // 错误写法，触发的时候才能运行
-    } else {
-      throw new TypeError("prompt must be string or function.");
-    } */
-
     if (typeof prompt !== "string" && typeof prompt !== "function") {
       throw new TypeError("prompt must be string or function.");
     }
@@ -23,23 +20,54 @@ export class BlockManager {
     return () => {
       this.prompt = null;
     }; // unBlock
+
+    /* if (typeof prompt === "string") {
+      this.prompt = prompt;
+     } else if (typeof prompt === "function") {
+      this.prompt = prompt(location, action); // 错误写法，触发的时候才能运行
+     } else {
+      throw new TypeError("prompt must be string or function.");
+     } */
   }
 
+  /**
+   * 触发阻塞
+   * @param {object} location
+   * @param {string} action
+   * @param {function} callback 当阻塞完成之后要做的事情（一般是跳转页面）
+   */
   triggerBlock(location, action, callback) {
-    if (this.prompt) {
-      let message;
-      if (typeof this.prompt === 'function') {
-        message = this.prompt()
-      } else {
-        message = this.prompt
-      }
-      this.getUserConfirmation(message, (isNext) => {
-        if (isNext) {
-          callback(true);
-        }
-      });
-    } else {
-      callback(true);
+    if (!this.prompt) {
+      callback();
+      return;
     }
+
+    let message;  // 阻塞消息
+    if (typeof this.prompt === "function") {
+      message = this.prompt(location, action);
+    } else if (typeof this.prompt === "string") {
+      message = this.prompt;
+    }
+    this.getUserConfirmation(message, (isNext) => {
+      if (isNext === true) {
+        // 可以跳转了
+        callback();
+      }
+    });
+    /* if (this.prompt) {
+       let message;
+       if (typeof this.prompt === 'function') {
+        message = this.prompt()
+       } else {
+        message = this.prompt
+       }
+       this.getUserConfirmation(message, (isNext) => {
+       if (isNext) {
+        callback(true);
+       }
+       });
+     } else {
+      callback(true);
+     } */
   }
 }
--- 6. Router源码/6.3 手写history：createBrowserHistory/react-learn/src/react-router/history/ListenManager.js
+++ 6. Router源码/6.3 手写history：createBrowserHistory/react-learn/src/react-router/history/ListenManager.js
@@ -1,8 +1,9 @@
 export class ListenManager {
+  // 存放监听器的数组
   listeners = [];
 
   /**
-   *
+   * 添加一个监听器，返回一个用于取消监听的函数
    * @param listener
    * @returns {(function())|*} 取消监听
    */
@@ -16,6 +17,11 @@ export class ListenManager {
     }; // unListen
   }
 
+  /**
+   * 触发所有的监听器
+   * @param {*} location
+   * @param {*} action
+   */
   triggerListener(location, action) {
     for (const listener of this.listeners) {
       listener(location, action);
