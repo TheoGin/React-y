349b3655f4981aeeba5119ca5beebb848ea3902d
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-01 17:32:39 +0800

    fix: 6.8 实现Link

--- 6. Router源码/6.8 实现Link/react-learn/src/react-router-dom/Link.js
+++ 6. Router源码/6.8 实现Link/react-learn/src/react-router-dom/Link.js
@@ -1,19 +1,23 @@
 import React from "react";
 import RouterContext from "../react-router/RouterContext";
 import { parsePath } from "history";
-
+// parsePath的作用，是根据一个路径字符串，返回一个location对象
 
 function Link(props) {
   let { to, children, ...rest } = props;
   // let href = "";
   let loc;
 
-  // 这样是不是会漏掉 basename
+
 
   if (typeof to === "string") {
-    /* href = history.createHref({
-     pathname: to,
+
+    /*
+     // 这样漏掉 basename
+     href = history.createHref({
+       pathname: to,
      }); */
+    // 将props.to转换为location对象
     loc = parsePath(to);
   } else if (typeof to === "object") {
     // href = history.createHref(to);
@@ -24,9 +28,10 @@ function Link(props) {
     <RouterContext.Consumer>
       {
         (value) => {
-          const href = value.history.createHref(loc);
+          const href = value.history.createHref(loc); // 控制台 element 展示用，需要 basename + url
           return <a href={ href } {...rest} onClick={ (e) => {
             e.preventDefault();
+            // value.history.push(href); // 错误写法，因为 push内部已经加了 basename
             value.history.push(loc);
           } }>{ children }</a>;
         }
--- 6. Router源码/6.8 实现Link/react-learn/src/react-router-dom/NavLink.js
+++ 6. Router源码/6.8 实现Link/react-learn/src/react-router-dom/NavLink.js
@@ -2,11 +2,11 @@ import React from "react";
 import Link from "./Link";
 import RouterContext from "../react-router/RouterContext";
 import { matchPath } from "../react-router/matchPath";
-import { createPath, parsePath } from "history";
+import { parsePath } from "history";
 
 function NavLink(props) {
   const {
-    children,
+    // children, // 往下传即可
     activeClass = "active",
     exact = false,
     strict = false,
@@ -17,21 +17,29 @@ function NavLink(props) {
   return (
     <RouterContext.Consumer>
       { value => {
-        let loc;
+        let loc;  // 保存to中的locaiton对象
         if (typeof props.to === "string") {
           loc = parsePath(props.to);
         } else if (typeof props.to === "object") {
           loc = props.to;
         }
         // const pathRule = value.history.createHref(loc);
-        const pathRule = createPath(loc);
-        const matchResult = matchPath(pathRule, {exact, sensitive, strict}, value.location.pathname);
-        console.log(pathRule);
-        console.log(loc);
-        console.log(matchResult);
-        return <Link { ...rest } className={ matchResult ? activeClass : '' }>{ children }</Link>;
-      }
-      }
+        // const pathRule = createPath(loc); // /page1?a=1#b=2
+
+        // 匹配不需要 path 和 hash
+        const pathRule = loc.pathname; // /page1
+        const matchResult = matchPath(pathRule, { exact, sensitive, strict }, value.location.pathname);
+
+        // console.log(loc.pathname, 'loc.pathname');
+        // console.log(createPath(loc), 'createPath(loc)');
+
+        // return <Link { ...rest } className={ matchResult ? activeClass : "" }>{ children }</Link>;
+
+        if (matchResult) {
+          return <Link { ...rest } className={ activeClass } />;
+        }
+        return <Link { ...rest }/>;
+      } }
     </RouterContext.Consumer>
   );
 }
