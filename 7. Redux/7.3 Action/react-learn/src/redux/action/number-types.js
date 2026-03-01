9b8f64a9f745016e8f32d2bc2ba3f6f34b8edf99
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-01 23:05:22 +0800

    feat: 7.3 Action

--- 7. Redux/7.3 Action/react-learn/src/redux/action/number-types.js
+++ 7. Redux/7.3 Action/react-learn/src/redux/action/number-types.js
@@ -1,6 +1,7 @@
 // 4. 在大型项目，由于操作类型非常多，为了避免硬编码（hard code），会将action的类型存放到一个或一些单独的文件中(样板代码)。
-export const INCREASE = "increase";
+// export const INCREASE = "increase";
+export const INCREASE = Symbol("increase"); // 即便是字符串一样，生成的 key 也是不一样的
 
-export const DECREASE = "decrease";
+export const DECREASE = Symbol("decrease");
 
-export const SET = "set";
+export const SET = Symbol("set");
