251ddec90674b17d478287daddd337db37dcf791
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-01 15:01:44 +0800

    fix: 6.6 实现Switch

--- 6. Router源码/6.6 实现Switch/react-learn/src/react-router/Switch.js
+++ 6. Router源码/6.6 实现Switch/react-learn/src/react-router/Switch.js
@@ -12,15 +12,27 @@ class Switch extends Component {
     );
   }
 
+  /**
+   * 循环children，得到第一个匹配的 Route 组件，若没有匹配，则返回null
+   */
   renderChildren = (ctx) => {
-    for (const child of this.props.children) {
+    // const children = Array.isArray(this.props.children) ? this.props.children : [this.props.children];
+    let children = [];
+    if (Array.isArray(this.props.children)) {
+      children = this.props.children;
+    } else if (typeof this.props.children === "object") {
+      children = [this.props.children];
+    }
+
+    for (const child of children) {
       // 不能使用 child.type instanceof Route，因为 child.type 就是 Route类本身
       if (child.type !== Route) {
-        console.warn('Warning: React does not recognize the `computedMatch` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `computedmatch` instead. If you accidentally passed it from a parent component, remove it from the DOM element.');
+        console.warn("Warning: React does not recognize the `computedMatch` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `computedmatch` instead. If you accidentally passed it from a parent component, remove it from the DOM element.");
       }
-      const matchResult = matchPath(child.props.path || '/', {}, ctx.location.pathname);
+
+      const matchResult = matchPath((child.props && child.props.path) || "/", {}, ctx.location.pathname);
       if (matchResult) {
-        return child
+        return child;
       }
     }
 
