426358e5d27accb5e444d4fd1d1502f3006a1274
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-01 12:46:50 +0800

    fix: 6.5 实现Route

--- 6. Router源码/6.5 实现Route/react-learn/src/react-router/Route.js
+++ 6. Router源码/6.5 实现Route/react-learn/src/react-router/Route.js
@@ -2,82 +2,16 @@ import React, { Component } from "react";
 import context from "./RouterContext";
 import { matchPath } from "./matchPath";
 
-
-/* class Route extends Component {
-
- renderContent = ({ location, history, match }) => {
- const {
- exact = false,
- sensitive = false,
- strict = false,
- children,
- component: Comp,
- } = this.props;
- const matchResult = matchPath(this.props.path, {
- exact,
- sensitive,
- strict,
- }, location.pathname);
- console.log(matchResult);
- const ctxValue = {
- location,
- history,
- match,
- };
-
- if (matchResult) {
- ctxValue.match = matchResult;
- if (children) {
- return (
- <context.Provider value={ ctxValue }>
- { children }
- </context.Provider>
- );
- } else if (Comp) {
- return <Comp {...ctxValue} />;
- }
- }
- return (
- <context.Provider value={ ctxValue }>
- { null }
- </context.Provider>
- );
- };
-
- render() {
-
- return (
- <context.Consumer>
- { this.renderContent }
- </context.Consumer>
- );
- }
- }
-
- export default Route; */
-
-
 class Route extends Component {
 
   renderContent = (ctxValue) => {
     const {
-      exact = false,
-      sensitive = false,
-      strict = false,
       children,
       component: Comp,
       render,
     } = this.props;
 
-    const matchResult = matchPath(this.props.path, {
-      exact,
-      sensitive,
-      strict,
-    }, ctxValue.location.pathname);
-    console.log(matchResult);
-
-    if (matchResult) {
-      ctxValue.match = matchResult;
+    if (ctxValue.match) {
       if (children) {
         return children;
       } else if (Comp) {
@@ -89,18 +23,51 @@ class Route extends Component {
     return null;
   };
 
+  matchRoute(ctxValue) {
+
+    const {
+      exact = false,
+      sensitive = false,
+      strict = false,
+    } = this.props;
+
+    return matchPath(this.props.path, {
+      exact,
+      sensitive,
+      strict,
+    }, ctxValue.location.pathname);
+  }
+
+  consumerFunc = (value) => {
+    /* const cxtValue = {
+      ...value,
+      match: this.matchRoute(value),
+    }; */
+    const cxtValue = {
+      history: value.history,
+      location: value.location,
+      match: this.matchRoute(value),
+    };
+    return (
+      <context.Provider value={ cxtValue }>
+        { this.renderContent(cxtValue) }
+      </context.Provider>
+    );
+  };
+
   render() {
 
     return (
       <context.Consumer>
-        { value => {
-          const cxtValue = { ...value };
-          return (
-            <context.Provider value={ cxtValue }>
-              { this.renderContent(cxtValue) }
-            </context.Provider>
-          );
-        } }
+        { this.consumerFunc }
+        {/* { value => {
+         const cxtValue = { ...value };
+         return (
+         <context.Provider value={ cxtValue }>
+         { this.renderContent(cxtValue) }
+         </context.Provider>
+         );
+         } } */ }
       </context.Consumer>
     );
   }
