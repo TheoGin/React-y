6d811d0fb1ada950ce432f30aa88b73bb74e2967
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-03 22:50:42 +0800

    init: 7.7 手写bindActionCreators

---
+++ 7. Redux/7.7 手写bindActionCreators/react-learn/src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from "../redux";
+import reducer from "./reducer";
+import { createAddUserAction, createDeleteUserAction, createUpdateUserAction } from "./action/userAction";
+import { createSetLoginUserAction } from "./action/loginUserAction";
+import { v4 as uuid } from "uuid";
+
+const store = createStore(reducer);
+// console.log(store);
+// console.log(store.getState());
+
+console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("触发subscribe store.getState()", store.getState());
+});
+
+store.dispatch(createDeleteUserAction(1));
+unsubscribe();
+store.dispatch(createSetLoginUserAction({ id: uuid(), name: "用户 login", age: 1 }));
+
+console.log(store.getState());
