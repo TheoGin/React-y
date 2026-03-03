669bae1a72be130a47a186a6cccb543bd416f2fc
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-03 21:50:17 +0800

    fix: 7.5 Store

--- 7. Redux/7.5 Store/react-learn/src/store/index.js
+++ 7. Redux/7.5 Store/react-learn/src/store/index.js
@@ -4,27 +4,18 @@ import { createAddUserAction, createDeleteUserAction, createUpdateUserAction } f
 import { createSetLoginUserAction } from "./action/loginUserAction";
 import { v4 as uuid } from "uuid";
 
-/* const initState = {
- loginUser: { id: uuid(), name: "用户 login", age: 1 },
- users: [
- { id: 1, name: "用户1", age: 18 },
- { id: uuid(), name: "用户2", age: 16 },
- ],
- };
- console.log(initState);
-
- const store = createStore(reducer, initState); */
 const store = createStore(reducer);
 
-// console.log(store.getState());
+console.log(store.getState());
 
-// store.dispatch(createAddUserAction({ id: uuid(), name: "用户3", age: 20 }));
-// store.dispatch(createUpdateUserAction(1, { id: 1, name: "用户 update", age: 22 }));
-
-store.subscribe(() => {
+const unsubscribe = store.subscribe(() => {
   console.log("触发subscribe store.getState()", store.getState());
 });
+
 store.dispatch(createDeleteUserAction(1));
+
+unsubscribe();  // 取消订阅
+
 store.dispatch(createSetLoginUserAction({ id: uuid(), name: "用户 login", age: 1 }));
 
 // console.log(store.getState());
