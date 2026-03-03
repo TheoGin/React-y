c1c59594b7f5a0eeafb525e0e992c65e1374544d
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-03 22:45:36 +0800

    fix: 7.6 手写createStore

--- 7. Redux/7.6 手写createStore/react-learn/src/redux/createStore.js
+++ 7. Redux/7.6 手写createStore/react-learn/src/redux/createStore.js
@@ -1,37 +1,59 @@
-function randomStrByLength(length) {
-  return "@@redux/INIT" + Math.random().toString(36).substr(2, length).split("").join(".");
+/**
+ * 得到一个指定长度的随机字符串
+ * @param {*} length
+ */
+function getRandomStringByLength(length) {
+  // return "@@redux/INIT" + Math.random().toString(36).substr(2, length).split("").join(".");
+  return Math.random().toString(36).substr(2, length).split("").join(".");
 }
 
-function isPlainObject(action) {
-  // return action.__proto__ === Object.prototype;
-  return Object.getPrototypeOf(action) === Object.prototype;
+/**
+ * 判断某个对象是否是一个plain-object
+ * @param {*} obj
+ */
+function isPlainObject(obj) {
+  if (typeof obj !== "object") {
+    return false;
+  }
+  // return obj.__proto__ === Object.prototype;
+  return Object.getPrototypeOf(obj) === Object.prototype;
 }
 
 export default function createStore(reducer, defaultState) {
+  let currentReducer = reducer, // 当前使用的reducer
+    currentState = defaultState; // 当前仓库中的状态
 
-  if (typeof reducer !== "function") {
-    throw new TypeError("reducer must be a function");
-  }
-  const type = randomStrByLength(6);
-  let state = reducer(defaultState, { type });
-  const listeners = [];
+  const type = `@@redux/INIT${ getRandomStringByLength(6) }`;
+  const listeners = []; // 记录所有的监听器（订阅者）
+
+  // 创建仓库时，需要分发一次初始的action
+  // let state = currentReducer(defaultState, { type });
+  dispatch({ type }); // 用 dispatch 减少重复代码
 
   // 1. dispatch：分发一个action
   function dispatch(action) {
-    if (typeof action !== "object") {
-      throw new TypeError("action must be a object");
-    }
+    // 验证action
+    /* if (typeof action !== "object") {
+     throw new TypeError("action must be a object");
+     } */
     if (!isPlainObject(action)) {
       throw new TypeError("action must be a plain object");
     }
 
-    if (!("type" in action)) {
+    // 验证action的type属性是否存在
+    // if (!("type" in action)) {
       // if (!action.hasOwnProperty('type')) {
+      if (action.type === undefined) {
       throw new TypeError("action must have a property of type");
     }
 
-    state = reducer(state, action);
+    if (typeof currentReducer !== "function") {
+      throw new TypeError("reducer must be a function");
+    }
 
+    currentState = currentReducer(currentState, action);
+
+    // 运行所有的订阅者（监听器）
     for (const listener of listeners) {
       listener();
     }
@@ -40,16 +62,25 @@ export default function createStore(reducer, defaultState) {
 
   // 2. getState：得到仓库中当前的状态
   function getState() {
-    return state;
+    return currentState;
   }
 
   // 3. subscribe：注册一个监听器，监听器是一个无参函数，该分发一个action之后，会运行注册的监听器。该函数会返回一个函数，用于取消监听
+  /**
+   * 添加一个监听器（订阅器）
+   */
   function subscribe(listener) {
-    listeners.push(listener);
+    listeners.push(listener); // 将监听器加入到数组中
+    let isRemove = false; // 是否已经移除掉了
 
     return () => {
+      if  (isRemove) { // 也可以判断 index 是否等于 -1来 return
+        return; // 防止重复 unsubscribe，减少循环次数
+      }
+      // 将listener从数组中移除
       const index = listeners.indexOf(listener);
       listeners.splice(index, 1);
+      isRemove = true;
     }; // unsubscribe
   }
 
