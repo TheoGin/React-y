a921ed6b8d70abf3619ca1a75e5948a7092257c2
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-04 22:23:14 +0800

    fix: 7.7 手写bindActionCreators

--- 7. Redux/7.7 手写bindActionCreators/react-learn/src/redux/bindActionCreators.js
+++ 7. Redux/7.7 手写bindActionCreators/react-learn/src/redux/bindActionCreators.js
@@ -1,32 +1,39 @@
 
-export default function bindActionCreators(actionCreator, dispatch) {
+export default function bindActionCreators(actionCreators, dispatch) {
   if (typeof dispatch !== "function") {
-    throw new Error("actionCreator must be a function");
+    throw new Error("actionCreators must be a function");
   }
 
-  if (typeof actionCreator === "function") {
-    return getActionAndDispatch(actionCreator, dispatch);
+  if (typeof actionCreators === "function") {
+    return getAutoDispatchActionCreator(actionCreators, dispatch); // 抽离共同代码
     /* return function (...args) {
-      const action = actionCreator(...args);
-      dispatch(action);
-    }; */
-  } else if (typeof actionCreator === "object") {
-    const result = {};
-    for (const key in actionCreator) {
-      const createAction = actionCreator[key];
-      result[key] = getActionAndDispatch(createAction, dispatch);
+     const action = actionCreators(...args);
+     dispatch(action);
+     }; */
+  } else if (typeof actionCreators === "object") {
+    const result = {}; // 返回结果
+    for (const key in actionCreators) {
+      if (actionCreators.hasOwnProperty(key)) {
+        const actionCreator = actionCreators[key]; // 取出对应的属性值
+        if (typeof actionCreator === "function") {
+          result[key] = getAutoDispatchActionCreator(actionCreator, dispatch);
+        } // 如果不是函数保留原值
+      }
       /* result[key] = function (...args) {
-        const action = createAction(...args);
-        dispatch(action);
-      }; */
+       const action = actionCreator(...args);
+       dispatch(action);
+       }; */
     }
     return result;
   } else {
-    throw new Error("actionCreator must be a function or object");
+    throw new Error("actionCreators must be a function or object");
   }
 }
 
-function getActionAndDispatch(actionCreator, dispatch) {
+/**
+ * 得到一个自动分发的action创建函数
+ */
+function getAutoDispatchActionCreator(actionCreator, dispatch) {
   return function (...args) {
     const action = actionCreator(...args);
     dispatch(action);
