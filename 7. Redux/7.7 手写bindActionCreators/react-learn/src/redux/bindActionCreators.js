12eede0f67656cbeb490755072555d251e9cfd47
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-04 22:17:00 +0800

    fix: 7.7 手写bindActionCreators

--- 7. Redux/7.7 手写bindActionCreators/react-learn/src/redux/bindActionCreators.js
+++ 7. Redux/7.7 手写bindActionCreators/react-learn/src/redux/bindActionCreators.js
@@ -1,24 +1,34 @@
+
 export default function bindActionCreators(actionCreator, dispatch) {
   if (typeof dispatch !== "function") {
     throw new Error("actionCreator must be a function");
   }
 
   if (typeof actionCreator === "function") {
-    return function (...args) {
+    return getActionAndDispatch(actionCreator, dispatch);
+    /* return function (...args) {
       const action = actionCreator(...args);
       dispatch(action);
-    };
+    }; */
   } else if (typeof actionCreator === "object") {
     const result = {};
     for (const key in actionCreator) {
       const createAction = actionCreator[key];
-      result[key] = function (...args) {
+      result[key] = getActionAndDispatch(createAction, dispatch);
+      /* result[key] = function (...args) {
         const action = createAction(...args);
         dispatch(action);
-      };
+      }; */
     }
     return result;
   } else {
     throw new Error("actionCreator must be a function or object");
   }
+}
+
+function getActionAndDispatch(actionCreator, dispatch) {
+  return function (...args) {
+    const action = actionCreator(...args);
+    dispatch(action);
+  };
 }
