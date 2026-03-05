b0d6a8a198744c10d959d24ed4d69cf9aa09256c
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-05 16:55:40 +0800

    fix: 7.9 Redux中间件（Middleware）

--- 7. Redux/7.9 Redux中间件/react-learn/src/store/index--dispatch实现原理.js
+++ 7. Redux/7.9 Redux中间件/react-learn/src/store/index--dispatch实现原理.js
@@ -5,18 +5,29 @@ import { createSetLoginUserAction } from "./action/loginUserAction";
 import { v4 as uuid } from "uuid";
 
 const store = createStore(reducer);
-const originDispatch = store.dispatch;
+let oldDispatch = store.dispatch; // 保留原本的dispatch函数
 
 // 实现Redux中间件的基本原理，是更改仓库中的dispatch函数。
-store.dispatch = function (action) {
-  console.log('rewrite dispatch: action', action);
-  console.log('rewrite dispatch: state before update', store.getState());
+store.dispatch = function (action) { // 更改store中的dispatch
+  console.log('rewrite dispatch1: action', action);
+  console.log('rewrite dispatch1: state before update', store.getState());
 
-  originDispatch(action);
+  oldDispatch(action);
 
-  console.log('rewrite dispatch: state after update', store.getState());
+  console.log('rewrite dispatch1: state after update', store.getState());
 };
+
+// oldDispatch = store.dispatch; // 死循环
+store.dispatch = function (action) { // 更改store中的dispatch
+  console.log('rewrite dispatch2: action', action);
+  console.log('rewrite dispatch2: state before update', store.getState());
+
+  oldDispatch(action);
+
+  console.log('rewrite dispatch2: state after update', store.getState());
+};
+
+// 这种方案只能写一个中间件，多个后面会把前面的给覆盖
 
 store.dispatch(createDeleteUserAction(1));
 store.dispatch(createSetLoginUserAction({ id: uuid(), name: "用户 login", age: 1 }));
-
--- 7. Redux/7.9 Redux中间件/react-learn/src/store/index.js
+++ 7. Redux/7.9 Redux中间件/react-learn/src/store/index.js
@@ -1,5 +1,5 @@
-import { createStore } from "../redux";
-import { applyMiddleware } from "redux";
+// import { createStore } from "../redux";
+import { applyMiddleware, createStore } from "redux";
 import reducer from "./reducer";
 import { createDeleteUserAction } from "./action/userAction";
 import { createSetLoginUserAction } from "./action/loginUserAction";
@@ -42,7 +42,11 @@ function loggerDispatch1(store) {
   };
 }
 
-function loggerDispatch2(store) {
+/**
+ * 一个中间件函数
+ * @param {*} store
+ */
+/* function loggerDispatch2(store) {
   console.log("loggerDispatch1 store", store); // loggerDispatch1 store {getState: ƒ, dispatch: ƒ}
 
   // 2. 中间件函数必须返回一个dispatch创建函数
@@ -56,11 +60,21 @@ function loggerDispatch2(store) {
       console.log("loggerDispatch2 state after update", store.getState());
     };
   };
+} */
+// 简写
+const loggerDispatch2 = store => nextDispatch => action => {
+  console.log("loggerDispatch2 action", action);
+  console.log("loggerDispatch2 state before update", store.getState());
+
+  nextDispatch(action);
+
+  console.log("loggerDispatch2 state after update", store.getState());
 }
 
 // 3. applyMiddleware函数，用于记录有哪些中间件，它会返回一个函数
 //   - 该函数用于记录创建仓库的方法，然后又返回一个函数
 
+// 应用中间件，
 // 一、applyMiddleware 结合 createStore 用法1：
 // const store = createStore(reducer, applyMiddleware(loggerDispatch1, loggerDispatch2));
 
