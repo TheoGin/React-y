07d4eca6298befeddb782c627c1213f399d61cff
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-07 21:49:30 +0800

    init: 8.4 [demo]管理学生数据

--- 8. Redux中间件/8.4 [demo]管理学生数据/react-learn/src/store/action/loginUserAction.js
+++
@@ -1,8 +0,0 @@
-export const SET_LOGIN_USER_TYPE = Symbol("set-login-usersReducer");
-
-export function createSetLoginUserAction(user) {
-  return {
-    type: SET_LOGIN_USER_TYPE,
-    payload: user,
-  };
-}
