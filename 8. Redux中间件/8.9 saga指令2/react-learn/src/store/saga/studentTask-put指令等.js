ad23c27a3282d5fe99f3d76089e38ad67168d9a5
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-12 22:46:29 +0800

    fix: 8.9 saga指令2 使用cps指令改进回调风格异步处理
    - 实现mockStudentsByTraditionCallback模拟nodejs风格异步回调
    - 在fetchUsers中使用cps指令调用传统回调异步函数
    - 处理异步成功和失败，确保加载状态正确切换
    - 添加异常捕获和finally保证关闭loading状态
    - 移除错误示例代码和多余注释，优化代码结构
    - 监听fetchUsers动作触发异步请求任务

--- 8. Redux中间件/8.9 saga指令2/react-learn/src/store/saga/studentTask-put指令等.js
+++ 8. Redux中间件/8.9 saga指令2/react-learn/src/store/saga/studentTask-put指令等.js
@@ -4,16 +4,31 @@ import { takeEvery, put, select, apply } from "redux-saga/effects";
 import { getStudentsPageByKeywordAndSex } from "../../services/student";
 
 function* fetchUsers() {
+  // 设置为正在加载中
   yield put(createIsLoadingAction(true));
   // getAllStudents().then(() => { /* 这里无法用 yield，就无法用指令 put */ })
 
   // 5. select指令：用于得到当前仓库中的数据
   const value = yield select();
   // console.log(value); // {student: {…}, counter: 10}
-  // const resp = yield getStudentsPageByKeywordAndSex(value.student.condition);
+
+  /*
+  try {
+    // 当saga发现得到的结果是一个Promise对象，它会自动等待该Promise完成
+    // 完成之后，会把完成的结果作为值传递到下一次next
+    // 如果Promise对象被拒绝，saga会使用generator.throw抛出一个错误
+    const resp = yield getStudentsPageByKeywordAndSex(value.student.condition);
+    yield put(createSetStudentsAndTotalAction(resp.data, resp.total));
+  } catch (err) {
+   // err表示reject的内容
+    console.log(err);
+  } finally {
+    yield put(createIsLoadingAction(false));
+  }
+  */
 
   // 3. call(fn, ...args) 指令：【可能阻塞（如Promise会阻塞）】用于副作用（通常是异步）函数调用
-  // const resp = yield call(getStudentsPageByKeywordAndSex, value.student.condition); // 一般会用指令？
+  // const resp = yield call(getStudentsPageByKeywordAndSex, value.student.condition); // 一般会用指令？使用call指令，按照当前仓库中的条件
 
   // 4. apply(this, fn, args) 指令：【可能阻塞】用于副作用（通常是异步）函数调用
   const resp = yield apply(null, getStudentsPageByKeywordAndSex, value.student.condition);
