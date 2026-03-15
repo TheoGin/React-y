c60d0da285093d50be7ec808b433179af3a73803
傅朝俊 <fuchaojun@szkingdom.com>
2026-03-15 21:23:34 +0800

    fix: 8.12 手写saga2 call

--- 8. Redux中间件/8.12 手写saga2/react-learn/src/store/saga/index——call test.js
+++ 8. Redux中间件/8.12 手写saga2/react-learn/src/store/saga/index——call test.js
@@ -1,11 +1,13 @@
 import { call } from "../../redux-saga/effects";
 
 function asyncFunc(a, b) {
-  console.log("asyncFunc触发 a, b", a, b); // asyncFunc触发 a, b 123 456
+  // console.log("asyncFunc触发 a, b", this, a, b); // asyncFunc触发 a, b 123 456
+  console.log("asyncFunc触发 this, a, b", this, a, b); // {asyncFunc: ƒ} 123 456
   // return 'aaa';
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(111);
+      // resolve(111);
+      reject(new Error('失败'));
     }, 1000);
   });
 }
@@ -20,10 +22,12 @@ export default function* rootSaga() {
   // const result = yield call(asyncFunc, 123, 456);
 
   // call([context, fn], ...args)
-  const result = yield call(['abc', asyncFunc], 123, 456);
+  // const result = yield call(['abc', asyncFunc], 123, 456);
 
   // call([context, fnName], ...args)
-  console.log('result', result); // 111
+  const result = yield call([{ asyncFunc }, "asyncFunc"], 123, 456);
+  // console.log("result", result); // 111
+  console.log("result", result); // result Error: 失败
   console.log("saga 完成");
 }
 
